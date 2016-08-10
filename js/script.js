8e9839e4883f6f5619a162c0766740ca1107b1ac
Hector Romo <info@hectorromo.com>
2016-08-10 17:29:53 +0200

    updated product

--- js/script.js
+++ js/script.js
@@ -1,23 +1,17 @@
 (function(window) {
-    
 	$(document).on('onBeforeAddProductToCart', function(event) {		
-
 		if (event.result === false) { // if an event handler already returned false, return false
 			return false;
 		}
-
 		var product = event.product;
 
-		product.pwyw = $('#pricing-suggestions-select option:selected').val();
+		product.pwyw = $('[data-id="'+product.id+'"] option:selected').val();
 
 		if ( parseInt(product.pwyw, 10) >= parseInt(product.price, 10 ) ) {
 			product.price = product.pwyw;
 		} else {
-			console.log('PWYW - false');
 			return false;
 		}
-	
 	});
-
 })(window);
 
